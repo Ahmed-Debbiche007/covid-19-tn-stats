deef604c1979b66351f73b7cf3bcdac882d8c40d
wassef911 <1.wassef911@gmail.com>
2020-03-23 22:56:37 +0100

    changes

--- sc.js
+++ sc.js
@@ -15,10 +15,13 @@ fetch(
     b = parseInt(obj.latest_stat_by_country[0].active_cases);
     c = parseInt(obj.latest_stat_by_country[0].total_deaths);
     d = parseInt(obj.latest_stat_by_country[0].total_recovered);
+    f = parseInt(obj.latest_stat_by_country[0].serious_critical);
+
     var data = {
       labels: [
         "Total Cases",
         "Active Cases",
+        "Serious Critical",
         "Total Deaths",
         "Total Recovered"
       ],
@@ -30,7 +33,7 @@ fetch(
           borderWidth: 3,
           hoverBackgroundColor: "rgba(255, 255, 255,0.2)",
           hoverBorderColor: "rgba(255, 255, 255)",
-          data: [a, b, c, d]
+          data: [a, b, f, c, d]
         }
       ]
     };
