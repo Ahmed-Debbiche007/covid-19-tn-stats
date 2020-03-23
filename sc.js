0e1f4b88d012ad66954e9ca474c96e9c11ffd1a7
wassef ben ahmed <49594956+wassef911@users.noreply.github.com>
2020-03-23 22:07:58 +0100

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,64 @@
+fetch(
+  "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=tunisia",
+  {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+      "x-rapidapi-key": "d0d248d0a1mshcd1d6a6016c608ap1d8892jsnd365febdda89"
+    }
+  }
+)
+  .then(e => e.json())
+  .then(function (e) {
+    const obj = e;
+    a = parseInt(obj.latest_stat_by_country[0].total_cases);
+    b = parseInt(obj.latest_stat_by_country[0].active_cases);
+    c = parseInt(obj.latest_stat_by_country[0].total_deaths);
+    d = parseInt(obj.latest_stat_by_country[0].total_recovered);
+    var data = {
+      labels: [
+        "Total Cases",
+        "Active Cases",
+        "Total Deaths",
+        "Total Recovered"
+      ],
+      datasets: [
+        {
+          label: "Current Count",
+          backgroundColor: "rgba(255, 255, 255,0.4)",
+          borderColor: "rgba(255, 255, 255,0.4)",
+          borderWidth: 3,
+          hoverBackgroundColor: "rgba(255, 255, 255,0.2)",
+          hoverBorderColor: "rgba(255, 255, 255)",
+          data: [a, b, c, d]
+        }
+      ]
+    };
+
+    var options = {
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            stacked: true,
+            gridLines: {
+              display: true,
+              color: "rgb(255, 255, 255,0.2)"
+            }
+          }
+        ],
+        xAxes: [
+          {
+            gridLines: {
+              display: false
+            }
+          }
+        ]
+      }
+    };
+
+    Chart.Bar("chart", {
+      options: options,
+      data: data
+    });
+  });
